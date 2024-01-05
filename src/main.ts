6563f992c3c79780b4f336d52630d05f3690de6f
Yakov Vladimirov <yakov_255@mail.ruy>
2024-01-05 18:24:26 +0500

    rewrite to primeVue (works)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import App from './App.vue'
 import './registerServiceWorker'
 import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/lara-light-green/theme.css'
+import InputNumber from 'primevue/inputnumber'
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import Calendar from 'primevue/calendar'
 
 const app = createApp(App)
 app.use(PrimeVue)
+app.component('InputNumber', InputNumber)
+app.component('Button', Button)
+app.component('Dialog', Dialog)
+app.component('Calendar', Calendar)
+
+
 app.mount('#app')
